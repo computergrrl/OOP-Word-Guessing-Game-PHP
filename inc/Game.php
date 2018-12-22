8afa57da1f5af5a5d2c1e30bdfc349f0b6ce69d6
computergrrl <computergrrl@users.noreply.github.com>
2018-12-22 10:31:14 -0800

    Added Phase class and Game class and created empty methods

---
+++ inc/Game.php
@@ -0,0 +1,45 @@
+<?php
+
+class Game
+{
+
+  public function numberLost()
+  {
+
+  }
+
+  public function checkForWin()
+  {
+  }
+
+  public function checkForLose()
+  {
+
+  }
+
+  public function gameOver()
+  {
+
+  }
+
+  public function displayPhrase()
+  {
+
+  }
+
+  public function displayLetterKey()
+  {
+
+  }
+
+  public function displayKeyboard()
+  {
+
+  }
+
+  public function displayScore()
+  {
+
+  }
+
+}
---
+++ inc/Phrase.php
@@ -0,0 +1,30 @@
+<?php
+
+class Phrase
+{
+
+  public function getRandomPhrase()
+  {
+
+  }
+
+  public function getPhrase()
+  {
+
+  }
+
+  public function getCharacters()
+  {
+
+  }
+
+  public function getLetters()
+  {
+
+  }
+
+  public function checkLetters()
+  {
+
+  }
+}
